2219a554f5ad04dd5bc77498fe6c35fdbbf3f536
manjunadhb <manjunadhb@gmail.com>
2018-12-27 17:48:43 +0530

    This is the project for Pulse

---
+++ Pulse1807/Pulse1807/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pulse1807
+//
+//  Created by Manjunadh Bommisetty on 27/12/18.
+//  Copyright © 2018 Manjunadh Bommisetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createUI();
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func createUI()
+    {
+        
+    }
+
+
+}
+
